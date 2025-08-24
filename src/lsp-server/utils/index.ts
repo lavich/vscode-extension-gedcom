1f6e24388776440c273d374e39c9179325401314
Andrei <lobanov.andrey.lavich@gmail.com>
2025-08-24 08:54:59 +0300

    refactor: move files

--- src/lsp-server/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { levelIndent } from "./indent/levelIndent";
-export { levelFolding } from "./folding/levelFolding";
-export { legend, semanticTokens } from "./semantic";
-export { getDefinitionsAtPosition } from "./getDefinitionsAtPosition";
