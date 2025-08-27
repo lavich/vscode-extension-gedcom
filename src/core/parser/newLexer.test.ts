ed3320c3167449feb9380132520b2c89b04cf65c
Andrei <lobanov.andrey.lavich@gmail.com>
2025-08-27 10:22:52 +0300

    feat: add chevrotain lexer

---
+++ src/core/parser/newLexer.test.ts
@@ -0,0 +1,56 @@
+import { describe, it, expect } from "vitest";
+import { GedcomLexer } from "./newLexer";
+
+describe("lexer", () => {
+  it("parse level and tag", () => {
+    const { tokens, errors } = GedcomLexer.tokenize("1 BIRT");
+    expect(errors.length).toBe(0);
+    expect(tokens.length).toBe(2);
+  });
+
+  it("parse level, tag, value", () => {
+    const { tokens, errors } = GedcomLexer.tokenize("1 BIRT VALU E");
+    expect(errors.length).toBe(0);
+    expect(tokens.length).toBe(3);
+  });
+
+  it("parse new line with level, tag, value", () => {
+    const { tokens, errors } = GedcomLexer.tokenize(
+      "1 NAME John /Doe/\n1 NAME John /Doe/"
+    );
+    expect(errors.length).toBe(0);
+    expect(tokens.length).toBe(6);
+  });
+
+  it("parse level, pointer, tag", () => {
+    const { tokens, errors } = GedcomLexer.tokenize("1 @POINTER@ BIRT \n1 @POINTER@ BIRT");
+    expect(errors.length).toBe(0);
+    expect(tokens.length).toBe(6);
+  });
+
+  it("parse level, pointer, tag + error", () => {
+    const { tokens, errors } = GedcomLexer.tokenize("1 @POINTER@ BIRT er df");
+    expect(errors.length).toBe(2);
+    expect(tokens.length).toBe(3);
+  });
+
+  it("parse new level", () => {
+    const { tokens, errors } = GedcomLexer.tokenize("1 BIRT VALU E\n2 TEST e");
+    expect(errors.length).toBe(0);
+    expect(tokens.length).toBe(6);
+  });
+
+  it("parse SAMPLE", () => {
+    const SAMPLE = `0 @I1@ INDI
+1 NAME John /Doe/
+1 BIRT
+1 BIRT
+2 DATE 1 JAN 1900
+0 @I2@ INDI
+3 FAM @i2@
+1 NAME Jane /Doe/`;
+    const { tokens, errors } = GedcomLexer.tokenize(SAMPLE);
+    expect(errors.length).toBe(0);
+    expect(tokens.length).toBe(22);
+  });
+});
---
+++ src/core/parser/newLexer.ts
@@ -0,0 +1,78 @@
+import { createToken, CustomPatternMatcherFunc, Lexer } from "chevrotain";
+import { IMultiModeLexerDefinition } from "@chevrotain/types";
+
+enum TokenNames {
+  LEVEL = "LEVEL",
+  POINTER = "POINTER",
+  TAG = "TAG",
+  XREF = "XREF",
+  VALUE = "VALUE",
+}
+
+// --- Служебные ---
+const WhiteSpace = createToken({
+  name: "WhiteSpace",
+  pattern: /[ \t]+/,
+  group: Lexer.SKIPPED,
+});
+
+const Newline = createToken({
+  name: "Newline",
+  pattern: /\r?\n/,
+  group: Lexer.SKIPPED,
+  line_breaks: true,
+  push_mode: "main",
+});
+
+// --- GEDCOM ---
+const Level = createToken({
+  name: TokenNames.LEVEL,
+  pattern: /[0-9]+/,
+  start_chars_hint: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+});
+
+const Pointer = createToken({
+  name: TokenNames.POINTER,
+  pattern: /@[A-Za-z0-9]+@/,
+  start_chars_hint: ["@"],
+  push_mode: "hasPointer",
+});
+
+const TagWithoutPointer = createToken({
+  name: TokenNames.TAG,
+  pattern: /[A-Z0-9_]+/,
+  start_chars_hint: [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"],
+  push_mode: "hasNotPointer",
+});
+
+const TagWithPointer = createToken({
+  name: TokenNames.TAG,
+  pattern: /[A-Z0-9_]+/,
+  start_chars_hint: [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"],
+  pop_mode: true,
+});
+
+const Xref = createToken({
+  name: TokenNames.XREF,
+  pattern: /@[A-Za-z0-9]+@/,
+  start_chars_hint: ["@"],
+  pop_mode: true,
+});
+
+const Value = createToken({
+  name: TokenNames.VALUE,
+  pattern: /.+/,
+  line_breaks: false,
+  pop_mode: true,
+});
+
+export const lexerDefinition: IMultiModeLexerDefinition = {
+  defaultMode: "main",
+  modes: {
+    main: [Newline, WhiteSpace, Level, Pointer, TagWithoutPointer],
+    hasPointer: [WhiteSpace, TagWithPointer],
+    hasNotPointer: [Newline, WhiteSpace, Xref, Value],
+  },
+};
+
+export const GedcomLexer = new Lexer(lexerDefinition);
